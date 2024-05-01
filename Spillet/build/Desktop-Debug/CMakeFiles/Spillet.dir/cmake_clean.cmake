fb281ea36c771712dff2826ea03d2edf29833dbb
niko825b <nifra23@student.sdu.dk>
2024-05-01 12:08:56 +0200

    Opret

---
+++ Spillet/build/Desktop-Debug/CMakeFiles/Spillet.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Spillet.dir/main.cpp.o"
+  "CMakeFiles/Spillet.dir/main.cpp.o.d"
+  "Spillet"
+  "Spillet.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Spillet.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ Spillet/build/Desktop-Debug/CMakeFiles/Spillet.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for Spillet.
+# This may be replaced when dependencies are built.
